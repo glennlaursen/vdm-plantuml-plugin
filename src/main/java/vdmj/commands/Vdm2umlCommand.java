e4b7d486119285ae097dc4bec555dd5af6427c8d
Jonas Lund <jonas.chuck.lund@gmail.com>
2022-08-25 14:58:32 +0200

    Clean up

--- src/main/java/vdmj/commands/Vdm2umlCommand.java
+++ src/main/java/vdmj/commands/Vdm2umlCommand.java
@@ -1,27 +1,3 @@
-/*******************************************************************************
- *
- *	Copyright (c) 2022 Nick Battle.
- *
- *	Author: Nick Battle
- *
- *	This file is part of VDMJ.
- *
- *	VDMJ is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
- *
- *	VDMJ is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
- *
- *	You should have received a copy of the GNU General Public License
- *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
- *	SPDX-License-Identifier: GPL-3.0-or-later
- *
- ******************************************************************************/
-
 package vdmj.commands;
 
 import vdmj.commands.VDM2UML.Buffers;
@@ -34,6 +10,7 @@ import com.fujitsu.vdmj.tc.definitions.TCClassList;
 
 import dap.DAPMessageList;
 import dap.DAPRequest;
+import json.JSONObject;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -52,6 +29,8 @@ public class Vdm2umlCommand extends Command
 	@Override
 	public DAPMessageList run(DAPRequest request)
 	{	
+		String message = "";
+
 		TCPlugin tcPlugin = PluginRegistry.getInstance().getPlugin("TC");
 		TCClassList classes = tcPlugin.getTC();
 
@@ -65,7 +44,7 @@ public class Vdm2umlCommand extends Command
 		buildBoiler();
 		printPlant("./", buffers);
 
-		return new DAPMessageList();
+		return new DAPMessageList(request, new JSONObject("result", message));
 	}
 
 	public StringBuilder buildBoiler() 
