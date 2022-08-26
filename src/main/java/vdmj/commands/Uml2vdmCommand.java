4bbaf6abd77e9980e13a350afe126688cde5bc88
Jonas Lund <jonas.chuck.lund@gmail.com>
2022-08-26 14:21:09 +0200

    Uses plantuml methods to create xmi

--- src/main/java/vdmj/commands/Uml2vdmCommand.java
+++ src/main/java/vdmj/commands/Uml2vdmCommand.java
@@ -1,9 +1,11 @@
 package vdmj.commands;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.OutputStream;
 
 import dap.DAPMessageList;
 import dap.DAPRequest;
@@ -18,8 +20,22 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import net.sourceforge.plantuml.BlockUml;
+import net.sourceforge.plantuml.ISkinSimple;
+import net.sourceforge.plantuml.LineLocationImpl;
+import net.sourceforge.plantuml.LineLocation;
 import net.sourceforge.plantuml.Run;
+import net.sourceforge.plantuml.StringLocated;
+import net.sourceforge.plantuml.api.ThemeStyle;
+import net.sourceforge.plantuml.classdiagram.ClassDiagram;
+import net.sourceforge.plantuml.classdiagram.ClassDiagramFactory;
+import net.sourceforge.plantuml.code.ByteArray;
+import net.sourceforge.plantuml.core.UmlSource;
+import net.sourceforge.plantuml.xmi.XmiClassDiagramStar;
+
 
 
 public class Uml2vdmCommand extends Command {
@@ -36,16 +52,45 @@ public class Uml2vdmCommand extends Command {
 		}
 	}
 
+	public static List<StringLocated> convert(List<String> strings) {
+		final List<StringLocated> result = new ArrayList<>();
+		LineLocationImpl location = new LineLocationImpl("block", null);
+		for (String s : strings) {
+			location = location.oneLineRead();
+			result.add(new StringLocated(s, location));
+		}
+		return result;
+	}
+
 	@Override
 	public DAPMessageList run(DAPRequest request)
 	{
 		try 
 		{
 			File inputFile = new File(path);
-
+			List<String> source = new ArrayList<>();
 			
+			try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+				source = br.lines().collect(Collectors.toList());
+			}
 
+			List<StringLocated> sourceLocated = new ArrayList<>();
+			LineLocationImpl location = new LineLocationImpl("uml", null);
+			for (String s : source) {
+				location = location.oneLineRead();
+				sourceLocated.add(new StringLocated(s, location));
+			}
+			UmlSource umlSource = UmlSource.create(sourceLocated, false);
+			
+			ClassDiagramFactory factory = new ClassDiagramFactory();
+			ClassDiagram classDiagram = factory.createEmptyDiagram(ThemeStyle.LIGHT_REGULAR, umlSource, null);
 
+			XmiClassDiagramStar xmiDiagram = new XmiClassDiagramStar(classDiagram);
+
+			OutputStream os = new ByteArrayOutputStream();
+			xmiDiagram.transformerXml(os);
+			
+			System.out.println(os.toString());
 
 			/* DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
          	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
