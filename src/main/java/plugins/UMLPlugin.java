5eb3a37ffe9917e7d5545fc441c2cc497b6f72b1
idhugoid <hugodsmacedo@gmail.com>
2022-09-28 15:33:54 +0200

    Adds Analysis plugin

---
+++ src/main/java/plugins/UMLPlugin.java
@@ -0,0 +1,62 @@
+package plugins;
+
+import json.JSONObject;
+import rpc.RPCErrors;
+import rpc.RPCMessageList;
+import rpc.RPCRequest;
+import workspace.Diag;
+import workspace.EventHub;
+import workspace.EventListener;
+import workspace.events.LSPEvent;
+import workspace.events.UnknownTranslationEvent;
+import workspace.plugins.AnalysisPlugin;
+
+public class UMLPlugin extends AnalysisPlugin implements EventListener {
+
+	public UMLPlugin() {
+		super();
+	}
+
+	@Override
+	public String getName() {
+		return "UML";
+	}
+
+	@Override
+	public void init() {
+		EventHub.getInstance().register(UnknownTranslationEvent.class, this);
+	}
+
+	@Override
+	public RPCMessageList handleEvent(LSPEvent event) throws Exception {
+
+		if (event instanceof UnknownTranslationEvent)
+		{
+			UnknownTranslationEvent ute = (UnknownTranslationEvent)event;
+			
+			if (ute.languageId.equals("uml2vdm"))
+			{
+				return analyseUML2VDM(event.request);
+			}
+		}
+		
+		return null;	// Not handled
+	}
+
+
+	public RPCMessageList analyseUML2VDM(RPCRequest request)
+	{
+		try
+		{
+			JSONObject params = request.get("params");
+
+			return new RPCMessageList(request, new JSONObject("uri", params.toString()));
+		}
+		catch (Exception e)
+		{
+			Diag.error(e);
+			return new RPCMessageList(request, RPCErrors.InternalError, e.getMessage());
+		}
+	}
+
+}
